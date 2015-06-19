c5d1d4022a1d314348b6bcb8a225ffd64d7867b9
Aleksei Pestov <pestov.alexey@gmail.com>
2015-06-19 17:32:16 +0300

    Java Proxy example

---
+++ src/main/java/io/pestov/tutorial/proxy/Foo.java
@@ -0,0 +1,9 @@
+package io.pestov.tutorial.proxy;
+
+/**
+ * @author <a href="mailto:pestov.aleksey@alphaopen.com" >Aleksey Pestov</a>
+ */
+public interface Foo {
+
+    String bar(String s);
+}
---
+++ src/main/java/io/pestov/tutorial/proxy/ProxyTest.java
@@ -0,0 +1,28 @@
+package io.pestov.tutorial.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ *
+ * @author Aleksey Pestov
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+        TraceHandler traceHandler = new TraceHandler();
+        Foo fooProxyInstance = (Foo) Proxy.newProxyInstance(Foo.class.getClassLoader(), new Class[]{Foo.class}, traceHandler);
+        String result = fooProxyInstance.bar("me");
+        System.out.println(result);
+    }
+}
+
+class TraceHandler implements InvocationHandler {
+    
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        System.out.println("bar(" + args[0] + ")");
+
+        return "welcome";
+    }
+}
